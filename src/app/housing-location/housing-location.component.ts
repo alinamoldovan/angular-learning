6d63788042f39bb7af2f7254a4ccaca3767760c8
Alina <alina.moldovan12@gmai.com>
2024-07-17 22:14:44 +0300

    Add test data

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,9 +6,17 @@ import { HousingLocation } from './houseing-location';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
+
   `,
   styleUrls: ['./housing-location.component.css'],
 })
